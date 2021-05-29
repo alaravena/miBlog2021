db539c5947f17329ac210396f164fc77597ea40b
Alejandra Aravena Silva <al.aravena@profesor.duoc.cl>
2021-05-29 17:41:52 -0400

    diccionario de contexto con objetos

--- core/views.py
+++ core/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        super().__init__()
+
 # Create your views here.
 def home(request):
     lista = ["Chocolate", "Helado", "Humitas"]
+
+    hijo = Persona("Darth Vento Colitaparada", '1,5')
+
     contexto = {
         'nombre': 'Alejandra Aravena',
         'edad': 42,
-        'comidas': lista
+        'comidas': lista,
+        'hijo': hijo
     }
     return render(request, 'core/home.html', contexto)
