56a5c92e934b6563874043d16fbc15b6664725fa
Alejandra Aravena Silva <al.aravena@profesor.duoc.cl>
2021-05-29 17:36:16 -0400

    diccionario de contexto con lista

--- core/views.py
+++ core/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    lista = ["Chocolate", "Helado", "Humitas"]
     contexto = {
         'nombre': 'Alejandra Aravena',
-        'edad': 42
+        'edad': 42,
+        'comidas': lista
     }
     return render(request, 'core/home.html', contexto)
